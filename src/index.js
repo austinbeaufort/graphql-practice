e97664ed04e3daa5ffbf9b788b7fe7e88a4cd014
austinbeaufort <abeaufort@sescollc.com>
2019-07-25 15:16:45 -0400

    seperated schema into own file, added mutation functionality.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-type Query {
-    info: String!
-}`
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'fullstack tutorial for graphql'
+}]
+
+let idCount = links.length;
 
 const resolvers = {
     Query: {
-        info: () => `This is an API practice test`
+        info: () => `This is an API practice test`,
+        feed: () => links,
+    },
+    Mutation: {
+        post: (parents, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link);
+            return link;
+        },
     }
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './schema.graphql',
     resolvers,
 });
 
